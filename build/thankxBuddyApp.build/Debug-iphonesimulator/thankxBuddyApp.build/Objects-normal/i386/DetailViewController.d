217857cf046148bfacfcec9808e47c74fae2f255
John LaBarge <johnlabarge@gmail.com>
2012-04-21 17:31:52 -0400

    add new stuff to project

---
+++ build/thankxBuddyApp.build/Debug-iphonesimulator/thankxBuddyApp.build/Objects-normal/i386/DetailViewController.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Developer/modevhackathon/thankxBuddyApp/DetailViewController.m \
+  /Developer/modevhackathon/thankxBuddyApp/DetailViewController.h
--- build/thankxBuddyApp.build/Debug-iphonesimulator/thankxBuddyApp.build/Objects-normal/i386/ModevUXSecondViewController.d
+++ build/thankxBuddyApp.build/Debug-iphonesimulator/thankxBuddyApp.build/Objects-normal/i386/ModevUXSecondViewController.d
@@ -1,3 +1,4 @@
 dependencies: \
   /Developer/modevhackathon/thankxBuddyApp/ModevUXSecondViewController.m \
-  /Developer/modevhackathon/thankxBuddyApp/ModevUXSecondViewController.h
+  /Developer/modevhackathon/thankxBuddyApp/ModevUXSecondViewController.h \
+  /Developer/modevhackathon/thankxBuddyApp/DetailViewController.h
